448d06ba7023d80d46b5daa7d3b9953814a289ad
ggzor <30713864+ggzor@users.noreply.github.com>
2018-10-29 12:29:59 -0600

    Guardando main del paquetito

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,9 +18,10 @@ public class App {
 
     Busqueda busqueda = new Busqueda(arreglo);
   
+    boolean salir = false;
     do {
-      System.out.println("\t\t\t Menú: \nSeleccione una opción:\n" + "\t 1.-Buscar la imformacion de un paciente por nombre.\n"
-      + "\t 2.-Buscar el nombre de los paciente que acudieron \n" + "\t a una consulta en determinada fecha.\n"
+      System.out.println("\n\nMenú: \nSeleccione una opción:\n" + "\t 1.-Buscar la información de un paciente por nombre.\n"
+      + "\t 2.-Buscar el nombre de los paciente que acudieron \n" + "\t    a una consulta en determinada fecha.\n"
       + "\t 3.-Buscar por número de expediente.\n" + "\t 4.-Número de cada tipo de pacientes. \n "
       + "\t 0.-Salir. \n ");
 
@@ -29,6 +30,9 @@ public class App {
       opcion.nextLine();
 
       switch (entrada) {
+      case 0:
+        salir = true;
+        break;
       case 1:
       //por nombre
         System.out.println("Ingrese el nombre: ");
@@ -44,12 +48,11 @@ public class App {
       case 2:
       //por fecha
         System.out.println("Ingrese la fecha: ");
-        String fechaBuscada = opcion.next();
-        opcion.nextLine();
+        String fechaBuscada = opcion.nextLine();
         ArrayList<Nombre> encontrados = busqueda.buscarPorFechaConsultaDeterminada(fechaBuscada);
          
-        if (encontrados == null){
-          System.out.println("Nombre no encontrado");
+        if (encontrados.size() == 0){
+          System.out.println("No acudieron pacientes en esa fecha.");
         }else{
           for(int i = 0; i < encontrados.size(); i++) {
             System.out.println(encontrados.get(i).toString());
@@ -70,37 +73,41 @@ public class App {
 
         break;
       case 4:
-        System.out.println(
-            "\t\t\t Menú: \nSeleccione una opción:\n" + "\t 1.-Número de pacientes diabeticos e hipertensos.\n"
+        boolean salirInterno = false;
+        do { 
+              System.out.println("\n\nMenú: \nSeleccione una opción:\n" + "\t 1.-Número de pacientes diabeticos e hipertensos.\n"
                 + "\t 2.-Número de pacientes menores de 12 años \n" + "\t 3.-Número de pacientes regulares.\n"
                 + "\t 0.-Salir. \n ");
-        do {
-          switch (opcion.nextInt()) {
+          System.out.println("Opcion: ");
+          int opcionInterna = opcion.nextInt();
+          opcion.nextLine();
+          switch (opcionInterna) {
           case 1:
             int contador1 = busqueda.contarNumeroPacientesDiabeticos();
-            System.out.println("El número total de pacientes diabeticos es:" + contador1);
+            System.out.println("El número total de pacientes diabeticos es: " + contador1);
             break;
           case 2:
             int contador2 = busqueda.contarNumeroPacientesMenoresEdad();
-            System.out.println("El número total de pacientes menores de 12 años es:" + contador2);
+            System.out.println("El número total de pacientes menores de 12 años es: " + contador2);
             break;
           case 3:
             int contador3 = busqueda.contarNumeroPacientesRegulares();
-            System.out.println("El número total de pacientes regulares es:" + contador3);
+            System.out.println("El número total de pacientes regulares es: " + contador3);
             break;
           default:
             System.out.println("Opcion Inválida.");
             break;
           case 0:
+            salirInterno = true;
             System.out.println("adiós :)");
             break;
           }
-        } while (opcion.nextInt() != 0);
+        } while (!salirInterno);
       default:
         System.out.println("Opcion Inválida.");
         break;
       }
-    } while (opcion.nextInt() != 0);
+    } while (!salir);
     }catch(FileNotFoundException fe) {
       System.out.println("No se encontro el archivo"+ fe.toString());
     } catch (Exception re) {
