fc1b620d66e63ce66d4640051bdd9ca52cff8565
ggzor <30713864+ggzor@users.noreply.github.com>
2018-10-29 10:11:36 -0600

    Agregando toString de ExpedienteDiabetico

--- src/main/java/ExpedienteDiabetico.java
+++ src/main/java/ExpedienteDiabetico.java
@@ -11,4 +11,17 @@ public class ExpedienteDiabetico extends Expediente {
   public Vector<ConsultaGlucosa> getRegistroGlucosa() {
     return registroGlucosa;
   }
+
+  public String toString() {
+    StringBuilder registroGlucosaCadena = new StringBuilder();
+
+    for (int i = 0; i < registroGlucosa.size(); i++) {
+      registroGlucosaCadena.append("  - ");
+      registroGlucosaCadena.append(registroGlucosa.get(i));
+      registroGlucosaCadena.append('\n');
+    }
+
+    return super.toString() + "\n"
+         + "Registro de glucosa: \n" + registroGlucosaCadena;
+  }
 }
